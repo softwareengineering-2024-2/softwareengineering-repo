908e2b07a89395556d764084dd3fcb97860ea700
ujeong03 <yhim0320@gmail.com>
2024-10-09 14:58:28 +0900

    로그인 연습

--- app.py
+++ app.py
@@ -1,9 +1,25 @@
-from flask import Flask,request
+import os
+from flask import Flask, render_template
+from controllers.auth import auth_bp, init_login_manager  # auth.py에서 가져오기
+from dotenv import load_dotenv
+ 
+# .env 파일 로드
+load_dotenv()
+
+# Flask app setup
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
-@app.route('/')
-def main():
-    return 'Hello World'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+# LoginManager 초기화
+init_login_manager(app)
+
+# Register the authentication blueprint
+app.register_blueprint(auth_bp, url_prefix='/auth')
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)  # HTTP로 실행
