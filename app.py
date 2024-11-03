c52a52b7ae50ba4c6dde810a80b93fcdb2d4df0c
Youngchae Lee <113342297+dudcoco@users.noreply.github.com>
2024-11-03 23:54:19 +0900

    Revert "feat #19: 프로덕트 백로그 지정 기능 - 유저스토리/백로그 생성 및 저장 기본 로직"

--- app.py
+++ app.py
@@ -1,14 +1,8 @@
 import os
 from flask import Flask, render_template
-from controllers.auth import auth_bp, init_login_manager
-from views.project_main_view import project_main_bp
-from views.manage_project_view import manage_project_bp
-from views.milestone_view import milestone_bp
-from views.productbacklog_view import productbacklog_bp
+from controllers.auth import auth_bp, init_login_manager  # auth.py에서 가져오기
 from dotenv import load_dotenv
-#from models import init_db
-from database import init_db
-
+ 
 # .env 파일 로드
 load_dotenv()
 
@@ -16,70 +10,16 @@ load_dotenv()
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
 
-# MySQL 데이터베이스 연결 설정
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-# 모델 초기화 함수 호출
-init_db(app)
 
 # LoginManager 초기화
 init_login_manager(app)
 
 # Register the authentication blueprint
 app.register_blueprint(auth_bp, url_prefix='/auth')
-app.register_blueprint(project_main_bp, url_prefix='/project_main')
-app.register_blueprint(manage_project_bp, url_prefix='/manage_project')
-app.register_blueprint(milestone_bp, url_prefix='/milestone')
-app.register_blueprint(productbacklog_bp, url_prefix='/backlog')  # /backlog 경로에 등록
-
 
 @app.route("/")
 def index():
     return render_template("index.html")
-
-#------------------------------------------------
-#view로 이동 요청
-@app.route("/main")
-def main():
-    return render_template("base.html")
-
-@app.route("/milestone")
-def milestone():
-    return render_template("milestone.html")
-
-@app.route("/userstory")
-def userstory():
-    return render_template("userstory.html")
-
-@app.route("/backlog")
-def backlog():
-    return render_template("backlog.html")
-
-@app.route("/sprint")
-def sprint():
-    return render_template("sprint.html")
-
-@app.route("/board")
-def board():
-    return render_template("board.html")
-
-@app.route("/review")
-def review():
-    return render_template("review.html")
-
-@app.route('/project')
-def project():
-    return render_template('manage_project.html')
-
-@app.route('/calendar')
-def calendar():
-    return render_template('calendar.html')
-
-@app.route('/guide')
-def guide():
-    return render_template('guide.html')
-#-------------------------------------------------
 
 if __name__ == "__main__":
     app.run(debug=True)  # HTTP로 실행
