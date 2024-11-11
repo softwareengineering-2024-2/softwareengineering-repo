4c6114086a373687f5b23e094dbf78ac59351766
alpakalee <tjwns1400@gmail.com>
2024-11-11 14:40:30 +0900

    feat: 프로필 설정창 역할 선택시 드롭박스 닫기

--- static/js/profile.js
+++ static/js/profile.js
@@ -3,16 +3,22 @@ const label = document.querySelector(".label");
 const options = document.querySelectorAll(".optionItem");
 const selectBox = document.querySelector(".selectBox");
 const roleSubmit = document.querySelector(".roleSubmit");
+const optionList = document.querySelector(".optionList"); // 옵션 목록
 
 // 클릭한 옵션의 텍스트를 라벨 안에 넣음
 const handleSelect = (item) => {
   label.parentNode.classList.remove("active");
   label.innerHTML = item.textContent;
   roleSubmit.value = item.textContent;
+  selectBox.classList.remove("active"); // 드롭다운 닫기
 };
-// 옵션 클릭시 클릭한 옵션을 넘김
+
+// 옵션 클릭 시 클릭한 옵션을 넘김
 options.forEach((option) => {
-  option.addEventListener("click", () => handleSelect(option));
+  option.addEventListener("click", (event) => {
+    event.stopPropagation(); // 클릭 이벤트 전파 방지
+    handleSelect(option);
+  });
 });
 
 // selectBox 클릭 시 옵션 목록이 열림/닫힘
