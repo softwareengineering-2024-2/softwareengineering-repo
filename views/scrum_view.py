5062ab35ed45eafc731cee7ced8da470d9036fe0
leedyun <leedo573@naver.com>
2024-11-22 19:41:07 +0900

    feat: 스프린트 스크럼 다음 날짜로 이동 등 추가 기능

--- views/scrum_view.py
+++ views/scrum_view.py
@@ -1,4 +1,5 @@
 # scrum_view.py
+from datetime import datetime
 from operator import and_
 from models.project_model import UserProject, Project
 from flask import Blueprint, redirect, render_template, request, jsonify, url_for
@@ -9,6 +10,7 @@ from flask_login import current_user
 from controllers.project_controller import get_user_projects
 from controllers.burnup_controller import update_completed_backlog
 from database import db
+from flask_wtf.csrf import generate_csrf
 
 # Blueprint 객체 생성
 scrum_bp = Blueprint('scrum', __name__)
@@ -19,20 +21,55 @@ def scrum_view(project_id):
         return redirect(url_for('auth.login'))  # 로그인 페이지로 리디렉트
     
     project = Project.query.get(project_id)
+    if not project:
+        return render_template(
+            'scrum.html',
+            message='프로젝트를 찾을 수 없습니다.',
+            project=None,
+            userproject=[],
+            sprints=[],
+            backlogs_by_status={},
+            completion_percentage=0,
+            csrf_token=generate_csrf()
+        )
+    
     userprojects = get_user_projects()
     sprints = get_sprints_with_backlogs(project_id)
-    
-    # 선택된 스프린트 ID 가져오기 (쿼리 파라미터로 받거나 첫 번째 스프린트로 기본 설정)
+
+    # 스프린트가 없는 경우 처리
     if not sprints:
-        # 스프린트가 없는 경우 처리
-        return render_template('scrum.html', message='현재 프로젝트에 스프린트가 없습니다.', project=project, userproject=userprojects)
-    
+        return render_template(
+            'scrum.html',
+            message='현재 프로젝트에 스프린트가 없습니다.',
+            project=project,
+            userproject=userprojects,
+            sprints=[],
+            backlogs_by_status={},
+            completion_percentage=0,
+            csrf_token=generate_csrf()
+        )
+
     selected_sprint_id = request.args.get('sprint_id', default=(sprints[0]["sprint_id"] if sprints else None))
+    if selected_sprint_id is not None:
+        selected_sprint_id = int(selected_sprint_id)
     selected_sprint = Sprint.query.get(selected_sprint_id)
 
     if not selected_sprint:
         # 선택된 스프린트가 존재하지 않는 경우의 처리
-        return render_template('scrum.html', message='선택된 스프린트를 찾을 수 없습니다.', project=project, userproject=userprojects, sprints=sprints, backlogs_by_status={})
+        return render_template(
+            'scrum.html',
+            message='선택된 스프린트를 찾을 수 없습니다.',
+            project=project,
+            userproject=userprojects,
+            sprints=sprints,
+            backlogs_by_status={},
+            completion_percentage=0,
+            csrf_token=generate_csrf()
+        )
+    
+    # sprints 리스트에서 selected_sprint의 is_past_due 값을 가져옴
+    selected_sprint_dict = next((sprint for sprint in sprints if sprint['sprint_id'] == selected_sprint_id), None)
+    is_past_due = selected_sprint_dict['is_past_due'] if selected_sprint_dict else False
 
     # 선택된 스프린트의 백로그 아이템 가져오기, 사용자 정보 포함
     sprint_backlogs = (
@@ -54,36 +91,49 @@ def scrum_view(project_id):
         backlog.user_name = user.user_name
         backlogs_by_status[backlog.status].append(backlog)
 
-    return render_template('scrum.html', project=project, sprints=sprints, selected_sprint=selected_sprint, backlogs_by_status=backlogs_by_status, userproject=userprojects, completion_percentage=completion_percentage)
+    csrf_token = generate_csrf()
+
+    return render_template('scrum.html', project=project, sprints=sprints, selected_sprint=selected_sprint, backlogs_by_status=backlogs_by_status, userproject=userprojects, completion_percentage=completion_percentage, csrf_token=csrf_token, is_past_due=is_past_due)
 
 # 스프린트 백로그 상태 업데이트 API
 @scrum_bp.route('/update_sprint_backlog_statuses', methods=['POST'])
 def update_sprint_backlog_statuses():
     data = request.get_json()
     updated_backlogs = data.get('updated_backlogs', [])
-    done_count = 0 # 'Done'으로 변경된 백로그 수
+    sprint_ids = set()
+    done_count_map = {} # 각 스프린트별 'Done' 상태 변경 카운트 초기화
     
     for backlog_data in updated_backlogs:
         backlog_id = backlog_data.get('backlog_id')
         new_status = backlog_data.get('new_status')
         
-        # 스프린트 백로그 아이템 가져오기
         backlog_item = SprintBacklog.query.get(backlog_id)
         if backlog_item:
+            current_status = backlog_item.status
             backlog_item.status = new_status
-            # 'Done'으로 상태가 변경되었는지 확인
-            if backlog_item.status != 'Done' and new_status == 'Done':
-                done_count += 1
-            # 'Done'에서 다른 상태로 변경되었는지 확인
-            elif backlog_item.status == 'Done' and new_status != 'Done':
-                done_count -= 1
-            backlog_item.status = new_status
+            if current_status != 'Done' and new_status == 'Done':
+                done_count_map[backlog_item.sprint_id] = done_count_map.get(backlog_item.sprint_id, 0) + 1
+            elif current_status == 'Done' and new_status != 'Done':
+                done_count_map[backlog_item.sprint_id] = done_count_map.get(backlog_item.sprint_id, 0) - 1
+            sprint_ids.add(backlog_item.sprint_id)
         else:
             return jsonify({'success': False, 'message': f'Backlog item {backlog_id} not found'}), 404
     
-    db.session.commit()
-    # 완료된 백로그 수를 업데이트하는 로직 호출
-    sprint_id = SprintBacklog.find_by_id(backlog_id).sprint_id
-    project_id = Sprint.find_by_id(sprint_id).project_id
-    update_completed_backlog(project_id, done_count)
-    return jsonify({'success': True})
+    try:
+        # 변경사항 커밋
+        db.session.commit()
+
+        # 각 스프린트에 대해 상태 업데이트
+        for sprint_id in sprint_ids:
+            sprint = Sprint.query.get(sprint_id)
+            if sprint:
+                if done_count_map.get(sprint_id, 0) != 0:  # 변경된 'Done' 상태가 있을 경우에만 호출
+                    update_completed_backlog(sprint.project_id, done_count_map[sprint_id])
+            else:
+                return jsonify({'success': False, 'message': f'Sprint {sprint_id} not found'}), 404
+        
+        db.session.commit()  # 최종 커밋
+        return jsonify({'success': True}), 200
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({'success': False, 'message': str(e)}), 500
